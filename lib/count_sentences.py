a940c0164ec638e349c3564d5404970bb72629e2
Your Name <youremail@domain.com>
2024-12-01 01:51:15 +0300

    initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
+# class MyString:
+  
+#   pass
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        import re
+        # Use regex to count the number of sentences based on punctuation
+        sentences = re.split(r'[.!?]+', self.value)
+        # Filter out empty strings
+        sentences = [s for s in sentences if s.strip()]
+        return len(sentences)
+
+
